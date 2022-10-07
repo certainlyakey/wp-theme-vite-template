b2e5a25d2320c41d88ec244064afbf29e93a0e89
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-10-07 13:20:52 +0300

    Fixed PHP linting errors in the Vite enqueueing script

--- functions/theme-setup/register-vite-assets.php
+++ functions/theme-setup/register-vite-assets.php
@@ -1,67 +1,65 @@
 <?php
 /*
- * VITE development
- * Inspired by https://github.com/andrefelipe/vite-php-setup
- *
- */
-
-// dist subfolder - defined in vite.config.json
-define('DIST_DEF', 'dist');
-
-// defining some base urls and paths
-define('DIST_URI', get_template_directory_uri() . '/' . DIST_DEF);
-define('DIST_PATH', get_template_directory() . '/' . DIST_DEF);
-
-// js enqueue settings
-define('JS_DEPENDENCY', array()); // array('jquery') as example
-define('JS_LOAD_IN_FOOTER', true); // load scripts in footer?
-
-// deafult server address, port and entry point can be customized in vite.config.json
-define('VITE_SERVER', 'http://localhost:3000');
-define('VITE_ENTRY_POINT', '/main.js');
+* VITE development
+* Inspired by https://github.com/andrefelipe/vite-php-setup
+*
+*/
 
 // enqueue hook
-add_action( 'wp_enqueue_scripts', function() {
-    
-    if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
+add_action(
+  'wp_enqueue_scripts',
+  function() {
 
-        // insert hmr into head for live reload
-        function vite_head_module_hook() {
-            echo '<script type="module" crossorigin src="' . VITE_SERVER . VITE_ENTRY_POINT . '"></script>';
+    // dist subfolder - defined in vite.config.json
+    $dist_folder = 'dist';
+
+    // defining some base urls and paths
+    $dist_uri = get_template_directory_uri() . '/' . $dist_folder;
+    $dist_path = get_template_directory() . '/' . $dist_folder;
+
+    // js enqueue settings
+    $js_deps = [];
+
+    if ( $_ENV['IS_VITE_DEVELOPMENT'] && true == $_ENV['IS_VITE_DEVELOPMENT'] ) {
+
+      // insert hmr into head for live reload
+
+      add_action(
+        'wp_head',
+        function() {
+          // default server address, port and entry point can be customized in vite.config.json
+          $vite_server = 'http://localhost:3000';
+          $vite_entry_point = '/main.js';
+          $vite_url = $vite_server . $vite_entry_point;
+          echo '<script type="module" crossorigin src="' . esc_url( $vite_url ) . '"></script>';
         }
-        add_action('wp_head', 'vite_head_module_hook');        
+      );
 
     } else {
 
-        // production version, 'npm run build' must be executed in order to generate assets
-        // ----------
+      // production version, 'npm run build' must be executed in order to generate assets
 
-        // read manifest.json to figure out what to enqueue
-        $manifest = json_decode( file_get_contents( DIST_PATH . '/manifest.json'), true );
-        
-        // is ok
-        if (is_array($manifest)) {
-            
-            // get first key, by default is 'main.js' but it can change
-            $manifest_key = array_keys($manifest);
-            if (isset($manifest_key[0])) {
-                
-                // enqueue CSS files
-                foreach(@$manifest[$manifest_key[0]]['css'] as $css_file) {
-                    wp_enqueue_style( 'main', DIST_URI . '/' . $css_file );
-                }
-                
-                // enqueue main JS file
-                $js_file = @$manifest[$manifest_key[0]]['file'];
-                if ( ! empty($js_file)) {
-                    wp_enqueue_script( 'main', DIST_URI . '/' . $js_file, JS_DEPENDENCY, '', JS_LOAD_IN_FOOTER );
-                }
-                
-            }
+      // read manifest.json to figure out what to enqueue
+      $manifest = json_decode( file_get_contents( $dist_path . '/manifest.json' ), true );
 
+      if ( is_array( $manifest ) ) {
+
+        // get first key, by default it is 'main.js' but it can change
+        $manifest_key = array_keys( $manifest );
+        if ( isset( $manifest_key[0] ) ) {
+
+          // enqueue CSS files
+          foreach ( @$manifest[ $manifest_key[0] ]['css'] as $css_file ) {
+            wp_enqueue_style( 'main', $dist_uri . '/' . $css_file );
+          }
+
+          // enqueue main JS file
+          $js_file = @$manifest[ $manifest_key[0] ]['file'];
+          if ( !empty( $js_file ) ) {
+            wp_enqueue_script( 'main', $dist_uri . '/' . $js_file, $js_deps, '', true );
+          }
         }
-
+      }
     }
-
-
-});
+  }
+);
