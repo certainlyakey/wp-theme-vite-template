50424bed0a5e31c270c3bfdeef516f95ceaaeb9a
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-10-23 00:17:25 +0300

    Added JS linting to the project

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 console.log('main.js');
 
-// get styles
+// Get styles
 import './assets/css/styles.css';
 
-// get scripts
+// Get scripts
 import './assets/js/scripts.js';
 
-/*
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-*/
+`;
