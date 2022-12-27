a61abe3ac6f0622d32d294cb99e2fcc6596ea266
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2022-12-27 14:47:13 +0200

    Dequeue remaining Gutenberg global styles on frontend

--- functions/frontend-hooks.php
+++ functions/frontend-hooks.php
@@ -16,6 +16,7 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 function themeprefix_remove_wp_block_library_css() {
   wp_dequeue_style( 'wp-block-library' );
   wp_dequeue_style( 'wp-block-library-theme' );
+  wp_dequeue_style( 'global-styles' );
 }
 
 add_action( 'wp_enqueue_scripts', 'themeprefix_remove_wp_block_library_css', 100 );
