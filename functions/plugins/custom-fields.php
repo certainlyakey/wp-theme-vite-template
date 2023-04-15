f43b0c9b3579bf686c89c1db94518258bae561e3
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-04-15 21:59:16 +0300

    Ensure a dash is used as a separator in file names of custom field includes

--- functions/plugins/custom-fields.php
+++ functions/plugins/custom-fields.php
@@ -9,14 +9,14 @@ function themeprefix_acf_add_php_fields() {
 
   // if ( function_exists( 'acf_add_local_field_group' ) ) {
     // Add your field group registration code in a separate file, one group per file
-    // require_once( $fields_path . 'fields_group.php' );
+    // require_once( $fields_path . 'fields-group.php' );
 
     // File naming convention:
-    // fields per site — fields_global_* (example: fields_global_analytics.php)
-    // fields for a post of a specific post type — fields_posttype_{posttype-name} (example: fields_posttype_pages.php)
-    // fields for all posts of a specific post type  — fields_posttype-common_{posttype-name} (example: fields_posttype-common_posts.php)
-    // fields of a specific page template or home/front page — fields_page_{page-template-name} (example: fields_page_front-page.php)
-    // fields for a term of specific taxonomy — fields_taxonomy_{page-template-name} (example: fields_taxonomy_categories.php)
+    // fields per site — fields-global-* (example: fields-global-analytics.php)
+    // fields for a post of a specific post type — fields-posttype-{posttype-name} (example: fields-posttype-pages.php)
+    // fields for all posts of a specific post type  — fields-posttype-common-{posttype-name} (example: fields-posttype-common-posts.php)
+    // fields of a specific page template or home/front page — fields-page-{page-template-name} (example: fields-page-front-page.php)
+    // fields for a term of specific taxonomy — fields_taxonomy-{taxonomy-name} (example: fields-taxonomy-category.php)
   // }
 }
 
